f538f55630cbc1f50ef92605a78f9a4ced6a055c
hatredholder <the.great.justice@bk.ru>
2022-08-29 19:29:11 +0300

    updated template, added delete route, added flashes

--- weather_app/__init__.py
+++ weather_app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'e1bc9ace66f8f5c5f4679b05e4b4c4753c33d9e08ed'
 
 db = SQLAlchemy(app)
 
--- weather_app/routes.py
+++ weather_app/routes.py
@@ -1,22 +1,42 @@
-from flask import redirect, render_template, request, url_for
+from flask import flash, redirect, render_template, request, url_for
 
 import requests
 
-from weather_app import app
+from weather_app import app, db
 from weather_app.models import City
+
+
+def get_weather_data(city):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={ city }\
+            &units=metric&appid=271d1234d3f497eed5b1d80a07b3fcd1'
+    data_json = requests.get(url).json()
+    return data_json
 
 
 @app.route('/', methods=['POST'])
 def index_post():
-    new_city = request.form.get('city')
+    # Lower form input to avoid duplicates in db
+    new_city = (request.form.get('city')).lower()
 
+    err_msg = None
+
+    # Check if theres anything in the form
     if new_city:
+        # Check if city already exists in database
         if not City.query.filter_by(name=new_city).first():
-            # add_city_if_correct returns success message
-            # if city was added
-            # and failure message if not
-            # message = add_city_if_correct(new_city)
-            pass
+            # Check if city entered correctly
+            if get_weather_data(new_city)['cod'] == 200:
+                db.session.add(City(name=new_city))
+                db.session.commit()
+            else:
+                err_msg = 'Incorrect city name. Please try again'
+        else:
+            err_msg = 'This city is already added'
+
+    if err_msg:
+        flash(err_msg, 'error')
+    else:
+        flash('City added succesfully!')
 
     return redirect(url_for('index_get'))
         
@@ -24,23 +44,35 @@ def index_post():
 @app.route('/', methods=['GET'])
 def index_get():
     cities = City.query.all()
-        
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}\
-           &units=metric&appid=56fd34333212ace3296df963b083bd09'
 
     weather_data = []
 
     for city in cities:
 
-        json_data = requests.get(url.format(city.name)).json()
+        json_data = get_weather_data(city)
     
         data = {
             'city': city.name,
             'temperature': json_data['main']['temp'],
-            'description': json_data['weather'][0]['description'],
+            # Capitalize to make it look better
+            'description': json_data['weather'][0]['description'].capitalize(),
             'icon': json_data['weather'][0]['icon'],
         }
 
         weather_data.append(data)
 
-    return render_template('weather.html', weather_data=weather_data)
+    return render_template(
+        'weather.html',
+        # Reverse list to get newly added cities first
+        weather_data=reversed(weather_data),
+    )
+
+
+@app.route('/delete/<name>')
+def delete_city(name):
+    city = City.query.filter_by(name=name).first()
+    db.session.delete(city)
+    db.session.commit()
+
+    flash(f'Successfully deleted { city.name }', 'success')
+    return redirect(url_for('index_get'))
