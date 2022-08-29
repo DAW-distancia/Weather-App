df73b50164140a327580939ec02c5c5ad495fdce
hatredholder <the.great.justice@bk.ru>
2022-08-29 18:41:47 +0300

    linted code, added flake8 config

--- weather_app/routes.py
+++ weather_app/routes.py
@@ -1,35 +1,44 @@
+from flask import redirect, render_template, request, url_for
+
 import requests
-from flask import render_template, request
-from weather_app import app, db
 
-from .models import City
-
-
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        new_city = request.form.get('city')
-        if new_city:
-            new_city_obj = City(name=new_city)
-            db.session.add(new_city_obj)
-            db.session.commit()
+from weather_app import app
+from weather_app.models import City
 
 
+@app.route('/', methods=['POST'])
+def index_post():
+    new_city = request.form.get('city')
+
+    if new_city:
+        if not City.query.filter_by(name=new_city).first():
+            # add_city_if_correct returns success message
+            # if city was added
+            # and failure message if not
+            # message = add_city_if_correct(new_city)
+            pass
+
+    return redirect(url_for('index_get'))
+        
+
+@app.route('/', methods=['GET'])
+def index_get():
     cities = City.query.all()
         
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=56fd34333212ace3296df963b083bd09'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}\
+           &units=metric&appid=56fd34333212ace3296df963b083bd09'
 
     weather_data = []
 
     for city in cities:
 
-        r = requests.get(url.format(city.name)).json()
+        json_data = requests.get(url.format(city.name)).json()
     
         data = {
             'city': city.name,
-            'temperature': r['main']['temp'],
-            'description': r['weather'][0]['description'],
-            'icon':r['weather'][0]['icon']
+            'temperature': json_data['main']['temp'],
+            'description': json_data['weather'][0]['description'],
+            'icon': json_data['weather'][0]['icon'],
         }
 
         weather_data.append(data)
