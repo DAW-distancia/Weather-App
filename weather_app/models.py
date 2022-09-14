e0c951e67a120724d2133c3606e688c7fcf86ada
hatredholder <rattlingserpent@gmail.com>
2022-09-14 15:43:02 +0300

    updated City model, linted code

--- weather_app/models.py
+++ weather_app/models.py
@@ -2,7 +2,7 @@ from weather_app import db
 
 
 class City(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    city_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):
