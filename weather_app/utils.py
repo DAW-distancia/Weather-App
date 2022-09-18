171271f0286ef4d9f5d598f8c38d7bca4bc61295
hatredholder <rattlingserpent@gmail.com>
2022-09-18 16:29:55 +0300

    reformatted code

--- weather_app/utils.py
+++ weather_app/utils.py
@@ -2,7 +2,7 @@ import requests
 
 
 def get_weather_data(city):
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={ city }\
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}\
             &units=metric&appid=271d1234d3f497eed5b1d80a07b3fcd1'
 
     data_json = requests.get(url).json()
