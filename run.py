8e72105669ab4e6414d26f6044dd14d93850df99
hatredholder <rattlingserpent@gmail.com>
2022-10-22 19:59:30 +0300

    added create_all to run.py

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
-from weather_app import app
+from weather_app import app, db
 
 
 if __name__ == '__main__':
+    # Create database if one doesn't exist yet
+    db.create_all()
     app.run(debug=True)
+    
