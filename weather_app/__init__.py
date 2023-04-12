75841d71e74661d82b8ac4a97dbabdef4aa53bd8
Luis Miguel <morillas@gmail.com>
2023-04-12 09:45:29 +0200

    path sqlite

--- weather_app/__init__.py
+++ weather_app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 
 from flask_sqlalchemy import SQLAlchemy
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+
 
 # Initialize app
 app = Flask(__name__)
 
 # Set SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{BASE_DIR}/weather.db'
 
 # Turn off track modifications (it's deprecated)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
